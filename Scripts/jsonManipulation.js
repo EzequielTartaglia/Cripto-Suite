1b23b6f80e4e86f5cf4cd7319bb4fa4525c559f6
EzequielTartaglia <ezequieltartaglia@churchofjesuschrist.org>
2024-05-28 10:13:00 -0300

    Change names

--- Scripts/jsonManipulation.js
+++ Scripts/jsonManipulation.js
@@ -1,9 +1,23 @@
-fetch('https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/EzequielTartaglia/Cripto-Suite/main/Json/developer_info.json')
-        .then(response => response.json())
-        .then(data => {
-          const footer = document.getElementById('footer');
-          footer.innerHTML = `
-            <div class="credit"> Crypto Suite&#174; | Developed by <span class="credito"><a href="${data.linkedin}" target="_blank">${data.name}</a></span> | All rights reserved 2023&copy;</div>
-          `;
-        })
-        .catch(error => console.error('Error loading JSON:', error));
+   const url = 'https://raw.githubusercontent.com/EzequielTartaglia/Cripto-Suite/main/Json/developer_info.json';
+
+   fetch(url, {
+     mode: 'cors', 
+     cache: 'no-cache',
+     credentials: 'same-origin', 
+     headers: {
+       'Content-Type': 'application/json'
+     }
+   })
+   .then(response => {
+     if (!response.ok) {
+       throw new Error('Network response was not ok');
+     }
+     return response.json();
+   })
+   .then(data => {
+     const footer = document.getElementById('footer');
+     footer.innerHTML = `
+       <div class="credit"> Crypto Suite&#174; | Developed by <span class="credito"><a href="${data.linkedin}" target="_blank">${data.name}</a></span> | All rights reserved 2023&copy;</div>
+     `;
+   })
+   .catch(error => console.error('Error loading JSON:', error));
